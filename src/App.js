4c3fd24c52c55289e63bdca4e4c139250fae4cec
Jam52 <jamie.sajdak@gmail.com>
2020-12-02 09:59:46 -0500

    Add normalize css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App" data-testid="component-app">
-      <header className="App-header">
-        <h1>Loads of Notes</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App" data-testid="component-app">
+        <header className="App-header">
+          <h1>Loads of Notes</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;

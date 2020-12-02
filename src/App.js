3cc3e1290f083293eb1cf7aa51354ee327351499
Jam52 <jamie.sajdak@gmail.com>
2020-12-02 11:46:01 -0500

    Test for all note sections and pass

--- src/App.js
+++ src/App.js
@@ -3,15 +3,60 @@ import styles from './App.module.scss';
 import NoteCard from './components/NoteCard/NoteCard';
 
 class App extends Component {
+  state = {
+    notes: [
+      { title: 'testing testing', notes: ' some notes', priority: 1 },
+      { title: 'testing testing', notes: ' some notes', priority: 1 },
+      { title: 'testing testing', notes: ' some notes', priority: 2 },
+      { title: 'testing testing', notes: ' some notes', priority: 3 },
+    ],
+  };
+
+  returnPriorityNotes = (priority) => {
+    return this.state.notes
+      .filter((note) => note.priority === priority)
+      .map((note, index) => <NoteCard note={note} key={index} />);
+  };
+
   render() {
+    const highPriorityNotes = this.returnPriorityNotes(1);
+    const mediumPriorityNotes = this.returnPriorityNotes(2);
+    const lowPriorityNotes = this.returnPriorityNotes(3);
+
     return (
       <div data-testid="component-app" className={styles.container}>
         <header className="App-header">
           <h1 className={styles.header}>Loads of Notes</h1>
         </header>
-        <NoteCard
-          note={{ title: 'testing testing', notes: ' some notes', priority: 1 }}
-        />
+        <main>
+          <section
+            data-testid="note-card-container"
+            className={styles.notesSection}
+          >
+            <h2>High Priority</h2>
+            <div className={styles.notesSection_container}>
+              {highPriorityNotes}
+            </div>
+          </section>
+          <section
+            data-testid="note-card-container"
+            className={styles.notesSection}
+          >
+            <h2>Medium Priority</h2>
+            <div className={styles.notesSection_container}>
+              {mediumPriorityNotes}
+            </div>
+          </section>
+          <section
+            data-testid="note-card-container"
+            className={styles.notesSection}
+          >
+            <h2>Low Priority</h2>
+            <div className={styles.notesSection_container}>
+              {lowPriorityNotes}
+            </div>
+          </section>
+        </main>
       </div>
     );
   }
